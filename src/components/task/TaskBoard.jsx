9f18ba42cd25765ae8a7c6f5b5a87e102ba6bfae
SoUrAv <aansourav@gmail.com>
2024-01-17 01:37:32 +0600

    Task add, edit, delete feature added

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -15,9 +16,55 @@ const TaskBoard = () => {
   };
 
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [taskToEdit, setTaskToEdit] = useState(null);
+  const [emptyTaskList, setEmptyTaskList] = useState(false);
+
+  const handleAddEditTask = (newTask, isAdd) => {
+    if (isAdd) {
+      setTasks([...tasks, newTask]);
+    } else {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === newTask.id) {
+            return newTask;
+          }
+          return task;
+        })
+      );
+    }
+    setShowAddModal(false);
+    setEmptyTaskList(false);
+  };
+
+  const handleEditTask = (task) => {
+    setTaskToEdit(task);
+    setShowAddModal(true);
+  };
+
+  const handleCloseClick = () => {
+    setTaskToEdit(null);
+    setShowAddModal(false);
+  };
+
+  const handleDelete = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+    if (updatedTasks.length === 0) {
+      setEmptyTaskList(true);
+    }
+  };
+
+  const handleDeleteAll = () => {
+    setTasks([]);
+    setEmptyTaskList(true);
+  };
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && (
+        <AddTaskModal onCloseClick={handleCloseClick} taskToEdit={taskToEdit} onSave={handleAddEditTask} />
+      )}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask />
@@ -27,9 +74,14 @@ const TaskBoard = () => {
           className="rounded
             -xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16"
         >
-          <TaskActions />
+          <TaskActions onAddTask={() => setShowAddModal(true)} handleDeleteAll={handleDeleteAll} />
 
-          <TaskList tasks={tasks} />
+          <TaskList
+            tasks={tasks}
+            onEdit={handleEditTask}
+            handleDelete={handleDelete}
+            emptyTaskList={emptyTaskList}
+          />
         </div>
       </div>
     </section>
